883918e4415fea67347f0b0f9bbaa4398126a87f
Wanderer2014 <byv2005@mail.ru>
2019-07-14 14:51:14 +0400

    'launch'

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 #token  = '451384433:AAERxCg40m21wKolCQ_j41UYHsIHBaK7gVA'
-database_name = 'C:/EndorFine/University/Diplom/Test2/heroku-telegram-bot/game.db'
+database_name = 'L:/Hobby-Dev/PCBOT/ahtubabot/game.db'

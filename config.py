d13782af881580e48e249a60ad2e0fb179e50c9a
Wanderer2014 <byv2005@mail.ru>
2019-07-20 21:43:22 +0400

    'launch'

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # token  = ''
+token = os.environ['TELEGRAM_TOKEN']
 # database_name = ''
-database = 'dfpgvn029auv8v'
+# database = 'dfpgvn029auv8v'
 DATABASE_URL = os.environ['DATABASE_URL']
 # database_name = 'games.db'
